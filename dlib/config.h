4b12714d5d32e57b6614e3f7fea5715951eba8b6
Crnisamuraj <m1ivancevic@gmail.com>
2021-02-09 18:26:08 +0100

    Update config.h

--- dlib/config.h
+++ dlib/config.h
@@ -22,7 +22,7 @@
 // #define DLIB_USE_FFTW
 // #define DLIB_USE_BLAS
 // #define DLIB_USE_LAPACK
-#define DLIB_USE_CUDA
+#define DLIB_USE_CUDA true
 
 
 // Define this so the code in dlib/test_for_odr_violations.h can detect ODR violations
